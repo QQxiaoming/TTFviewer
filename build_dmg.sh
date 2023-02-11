ac09a16f5307bdebdb662a725c1df8ddd3d61922
xiaoming <2014500726@smail.xtu.edu.cn>
2023-02-11 23:11:33 +0800

    ci:update script
    Signed-off-by: xiaoming <2014500726@smail.xtu.edu.cn>

--- build_dmg.sh
+++ build_dmg.sh
@@ -7,7 +7,7 @@ QT_DIR=/opt/Qt6.2.0/6.2.0/gcc_64
 # 定义版本号
 TTFVIEWER_MAJARVERSION="0"
 TTFVIEWER_SUBVERSION="2"
-TTFVIEWER_REVISION="2"
+TTFVIEWER_REVISION="3"
 ###############################################################################
 
 
@@ -20,14 +20,14 @@ export QML2_IMPORT_PATH=$QT_DIR/qml
 TTFVIEWER_VERSION="V"$TTFVIEWER_MAJARVERSION$TTFVIEWER_SUBVERSION$TTFVIEWER_REVISION
 # 编译
 rm -rf .qmake.stash Makefile
-qmake -makefile
+$QT_DIR/bin/qmake -makefile
 make
 cp -R ./test ./build_release/out/TTFviewer.app/contents/resources/test
 cp ./tools/create-dmg/build-dmg.sh ./build_release/out/build-dmg.sh
 cp ./tools/create-dmg/installer_background.png ./build_release/out/installer_background.png
 cd ./build_release/out
 # 打包
-macdeployqt TTFviewer.app
+$QT_DIR/bin/macdeployqt TTFviewer.app
 otool -L ./TTFviewer.app/Contents/MacOS/TTFviewer
 ./build-dmg.sh TTFviewer
 echo build success!
--- partform_unix.pri
+++ partform_unix.pri
@@ -2,5 +2,5 @@
 # 定义freetype2 lib路径
 FREETPE2_DIR=/home/qqm/Desktop/ttfviewer/depend/freetype
 # 定义版本号路径
-TTFVIEWER_VERSION=0.2.2
+TTFVIEWER_VERSION=0.2.3
 ###############################################################################
